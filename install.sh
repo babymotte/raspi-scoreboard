31d6eab87935f13d167c4b4c7178efb0b98bf10e
Michael Bachmann <mbachmann@bbmsoft.net>
2023-02-04 00:53:06 +0100

    docker + static ip

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+sudo apt update && sudo apt -y full-upgrade && sudo apt autoremove
+
 # install docker
-# TODO
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh
 
 # install node-red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-
-# config dhcpcd
-sudo cp ./dhcpcd.conf /etc/dhcpcd.conf
 
 # install dhcp
 sudo apt-get install -y isc-dhcp-server
@@ -29,5 +28,8 @@ sudo chmod 600 /etc/hostapd/hostapd.conf
 
 # install scoreboard
 ./start.sh
+
+# static ip
+sudo cp ./wlan0.static /etc/network/interfaces.d/wlan0.conf
 
 sudo reboot
