ada5d59410546c2d9fe9d8e69b7e5f0107db2321
Michael Bachmann <mbachmann@bbmsoft.net>
2023-02-04 02:52:32 +0100

    final cleanup

--- install.sh
+++ install.sh
@@ -14,40 +14,39 @@
 curl -fsSL https://get.docker.com -o get-docker.sh &&
     sudo sh get-docker.sh &&
     rm get-docker.sh &&
-    sudo gpasswd -a gwoerch docker &&
-    sudo systemctl enable docker ||
+    sudo systemctl enable --now docker ||
     exit $?
 
 # install node-red
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) || exit $?
-cd $HOME/.node-red &&
+DIR=$(pwd)
+curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered >node-setup.sh &&
+    bash node-setup.sh --confirm-install --confirm-pi --restart --update-nodes &&
+    rm node-setup.sh &&
+    cd $HOME/.node-red &&
     npm install @babymotte/node-red-worterbuch &&
     npm install node-red-dashboard &&
     npm install node-red-node-pi-gpio &&
-    cd projects && git clone https://github.com/babymotte/scoreboard-flow.git &&
-    cd || exit $?
-sudo systemctl start node-red || exit $?
+    mkdir projects && cd projects && git clone https://github.com/babymotte/scoreboard-flow.git &&
+    cd "$DIR" || exit $?
 
 # install dhcp
-sudo apt-get install -y isc-dhcp-server || exit $?
-echo "INTERFACESv4=\"wlan0\"" | sudo tee /etc/default/isc-dhcp-server || exit $?
-sudo cp ./dhcpd.conf /etc/dhcp/dhcpd.conf || exit $?
-sudo systemctl start isc-dhcp-server || exit $?
+sudo apt-get install -y isc-dhcp-server &&
+    echo "INTERFACESv4=\"wlan0\"" | sudo tee /etc/default/isc-dhcp-server &&
+    sudo cp ./dhcpd.conf /etc/dhcp/dhcpd.conf || exit $?
 
 # install dns
-sudo apt install -y bind9 || exit $?
-sudo cp ./named.conf.options /etc/bind/named.conf.options || exit $?
-sudo cp ./named.conf.local /etc/bind/named.conf.local || exit $?
-sudo cp ./forward.local /etc/bind/forward.local || exit $?
-sudo systemctl restart bind9.service || exit $?
+sudo apt install -y bind9 &&
+    sudo cp ./named.conf.options /etc/bind/named.conf.options &&
+    sudo cp ./named.conf.local /etc/bind/named.conf.local &&
+    sudo cp ./forward.local /etc/bind/forward.local || exit $?
 
 # install access point
-sudo apt install -y hostapd || exit $?
-sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf || exit $?
-sudo chmod 600 /etc/hostapd/hostapd.conf || exit $?
-sudo cp ./hostapd.default /etc/default/hostapd || exit $?
-sudo systemctl unmask hostapd || exit $?
-sudo systemctl enable --now hostapd || exit $?
+sudo apt install -y hostapd &&
+    sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf &&
+    sudo chmod 600 /etc/hostapd/hostapd.conf &&
+    sudo cp ./hostapd.default /etc/default/hostapd &&
+    sudo systemctl unmask hostapd &&
+    sudo systemctl enable hostapd || exit $?
 
 # install scoreboard
 ./start.sh || exit $?
@@ -56,6 +55,6 @@ sudo systemctl enable --now hostapd || exit $?
 sudo apt -y autoremove
 
 # static ip
-sudo cp ./wlan0.static /etc/network/interfaces.d/wlan0.conf || exit $?
+sudo cp ./wlan0.conf /etc/network/interfaces.d/wlan0.conf || exit $?
 
 sudo reboot
